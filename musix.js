c73cf86b7e9d6a641297e41e32c9cc580e70fcfd
Raghavendra Technic <117390010+raghavendratechnic@users.noreply.github.com>
2023-09-14 23:34:11 +0530

    Create musix.js

---
+++ musix.js
@@ -0,0 +1,181 @@
+[{
+      name: "bAsti Ka Hasti",
+      artist: "mC _st@N",
+      cover: "https://c.saavncdn.com/591/Basti-Ka-Hasti-Hindi-2022-20220324065126-500x500.jpg",
+      source: "https://github.com/raghavendratechnic/Temporary/raw/main/Basti-Ka-Hasti-MC-STAN-slowed-reverb.mp3",      
+      },
+      {
+      name: "Open Your Wings",
+      artist: "Rohit Gopalakrishnan",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDAjtsBuE4NuuwYjfG6of3Jfid9l6yRdwb9YIt-zQHg_v0MyI-PqF1yKothxOgLaA1ViNnS6h_jwkRPTfhQoxiP0r8q0HvTiQN_eucqs6gu96LoT3tkOA_gHigySp7-ANyd3yaEcqlW-QFZXAxWDwyYLr0VrXTYBZkN62D6D3SWjdwB8U8Kvsmbx4C-A/s1600/images.jpeg",
+      source: "https://raw.githubusercontent.com/RTW06/musicPalyer/main/OpenYourWings.mp3",      
+      },
+      {
+      name: "Hope",
+      artist: "XXXTENTACION",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgjXGliWaxS_MYJitf2Av6ap7UTTh4bZE71aQX7X-6otNUdTPP-pcdNjuuc2S5QOauxCAFYUraHvxY3Q29esM3Iumui7F1OhFWlY40XMmNoLyEX06uH-a3PjefwdC0R7IkTbb6-LylsxgqJSbjokwNApRsN9TsJ_PcODIOHveuCmAvlSx8heoq0Kkgvig/s1600/maxresdefault.jpg",
+      source: "https://drive.google.com/uc?export=download&id=167jQqBNvgUikMpFG-JSW57GMgYxhLFR3"
+      },
+      {
+      name: "Maate Vinadhuga",
+      artist: "Sid Sriram",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgymAHacrXKHb8B4QeZY4p-k6--3eS8oYGo8tmqFrfo7Jyt5l5zyf4jfuNHOzJtgRB6jb1eAVcTWbZdsw82YM7_b4Pu2XMvVFMTE2qXQXirxIon-3enesDMM5MG4j2zrENJeg9Nl4AV1dZZLSUUPsmSpaKV2jeLiFL7sh6QpXBdw7UKHbSBHuelKDLzVw/s1600/images%20%282%29.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=10CxCp5rRbjGEovMZnfhoU9COS5sc8yqV"
+      },
+      {
+      name: "Kallaimattum Kandal",
+      artist: "Hariharan",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh7KX0fPFeGMD3ZB8iEb1vPKJ3BLAFSbxfqHeuF7JYt1rK7SYCCLTo-9MBIpKRB3WXmhZ-1v1bSsNj6LY5VY0efZC67eNcSJ4T8ocO7Y2U-KKY6zhEYtizlPVhcdU7oyKzIsgLNGHqgG-iMeN0QXPHgD93PMaWvu6wFew8Ysne9raxTa0BUmTtMBlfrPQ/s1600/images.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=1sIefTKtP8nHH4jEB1rZXXZjWvuFouCt-"
+      },
+      {
+      name: "lovely",
+      artist: "Billie Eilish, FINNEAS & Khalid",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgiR3P9cbnPOSwtN0obLdc51Egu_mhH9GkXS4iEDyw_--WXLl71a3rQFmERfbN0TImd0peXJH0f4B526svdHUNPjrbmC_Z8qHYrWt23p0pL29G1rWi3ihIAXRR6WVZyrEfjj7tBryyV6WsOP3y546GWhU5r3Z0xMnBBms1bzfEGAvVfcVbAY56GIsC9Ng/s1600/images.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=15ezyOfLO-XO5oMJDz50DzFbjPuGpsBmw"
+      },
+      {
+      name: "Undiporaadhey",
+      artist: "Sid Sriram",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGaxRQqQisLSTSaWUH08CGQ7JvlbO5aRVVLE_59RkLXxK-TDiGO1PaXNRPtP2T3wFNhhzlkz7-AEvKRSbmFwgj8YOMW7dFGR_lj5SV6vHhXTdBJmKyhjUeEqZXlIiJi6Pvsys0VGdZX97lEvsXzNIN2f61tIpT6b4ofCgcLdfWFW4q3hVLwpQetRlz/s1600/images.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=1lnndo9FwLyFU_PggAYrM3GMlpdv3vBF4"
+      },
+      {
+      name: "Pataas Pilla",
+      artist: "Anirudh Ravichander",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZ7yTYNhL_LmDw7fyA8FBhfgfYuI4OYQ30Z4uzw8XTI53UAu7agtbLniIUDdh-iCK-n21Gvd9ypRvv8k7eVU2fVcXVMGqJJja2Ekyek0L3JEU5xeDP8lzze0IY29ZbxDiysxK-ulI3NEKHhrMXY-zv-ex-jqPSIKPlABKFhSeK-iDSBZWrsT4688fUjQ/s1600/images%20%289%29.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=1jGGfuCDSh0OQWnM8Hg2H5QqR-OhPuMLf"
+      }, 
+      {
+      name: "Hoyna Hoyna",
+      artist: "Inno Genga",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGP7Dd5VBRK55cRYsCYGLaiPhrhomjkauLOAjzcFgTdEg6v8P0OUUY1eZHc8JMtxns1AGUgIanWgKzvqg8PY5ID-U80njp0s2OMGFyCHUTfIRyu2FXEsOpaDaaZP-leP0qd5fr8EhB_o6ubN86n21rMadx-L7g5KVs7Tg0t4z4DJ0_mr2yO5UKaEUTuQ/s1600/size_l_1565942474.webp",
+      source: "https://drive.google.com/uc?export=download&id=1ixzl8RAvuOCaJqd5zwkdR5_tO7DoiVa9"
+      },
+      {
+      name: "Oo Antava Mawa..",
+      artist: "Indravathi Chauhan",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDP-_WvLIJBOyFIVMg_z6qCzvsWVn_dpiI-QixUQpxyXs0MwcNTD2K3JtnkPXJ5orYiWZ5jYXWv6D9N0RrA6G18m2AY8W9sEAh1dvbSUi6TznzGgo_oc7WTFHsbY9e2W3_nTVoC3INZBtCFcDydQRkKjuxX5FvXEY36A5FULS0pb6PAqBMUPG4wAWTLQ/s1600/images%20%288%29.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=1inQhkpSQ0rJERuR0dK7eGqFDuqGtmfgw"
+      },
+      {
+      name: "Gundellonaa",
+      artist: "Anirudh Ravichander",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgITIkH7eOtc7tt9uCf3DjyHiLbLJvu9oMy9MVsIDuYjhtnmPUggWQZEJk2rWEipSZelx_eGEuSiWbyXxo9EoTsX6CTs3S08a1PLm-Zvn-Ryoeuf0O3WpEE6ORSqmpd4i1MioieKlVWVsrdNwiZapDuls_vNgDH_eBW3zzyUsgFjnLzqxpYgBnbhfIndw/s1600/Gundellonaa-Song-Ori-Devuda-Anirudh-Ravichander.jpg",
+      source: "https://drive.google.com/uc?export=download&id=1ikzSk6hjgmgIXlPbHzY-Gj1oaFsanNP2"
+      },
+      {
+      name: "Maya Chesesave",
+      artist: "Afroz Ali",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh1FshdJllkuV6xomKphM1FK6kNX6FQs22mkHxSC-XlVtZ6G6YSmS0MGvFoAPRoheiWgtwGYTShYDZKe7zGfmiKUpzVNyUfh3HVBx1Ph_wNwOAnmqg97A6LeCtCwWZ-xeEQKF5UFLaJ1mTUiCcFSZGTP-LI-q2H2DVG-qYGjueELUdBp30F-dH4yHrAMQ/s1600/images%20%286%29.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=1iaNJ-YMfSk3EAKjtD3D9wAzHoWpwe0Uk"
+      },
+      {
+      name: "Paagal Title Song",
+      artist: "Ram Miryala",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg3Xo-jbg7RwUc4qZMS7Ti5BKZU0WGlBN-2tJhQTlwfnc4tBQdrqFYaBa8DFVB3yPXZRXrn0lEFKGrcznkmVT3MYFCkLz4_CXupd5mcgK1X7PoVSh62tYNUqjBueCwpAUKzfH2jX4fHpRw8QTdSsKi_XTJzE65bxO2tsnv3VfKV3mVj47pET3bYOXNzkQ/s1600/paagal-photo-3.jpg",
+      source: "https://drive.google.com/uc?export=download&id=1iTIK_Xbvdb6nwO3M-vSX8FpjjXkJ2a6P"
+      }, 
+      {
+      name: "Saradaga Kasepaina",
+      artist: "Karthik",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgVgclWcIFPTEvAosr2-bQZ9GQLI2KB-BUNs0fCNRHXxKGPQBFKyhET5i39vjG6c7D-88AU_RgPW1eJLC4xV5bB6U16LMHmiyRU1gpih_D35aCu-TphXLLeBAbf0iMeTUJ53v60pBqZxyBjCCmBFkLPicyrytKsw5nLZoqYeZmkTIJWy0Fl5NfutH3zcg/s1600/20221113_014432.png",
+      source: "https://drive.google.com/uc?export=download&id=1iP3gw7gnh-Aq4yRs_th7_tVXaK5yFS0R"
+      },
+      {
+      name: "Oh Sita Hey Rama",
+      artist: "Vishal Chandrasekhar",
+      cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgBNUvgC_Wa5rvruH6EEhaWs0IUrD30pAEb4CBip25yDPwApThlyQg5HhqkXiNCI6BEVDhzwCv6aSlzRZpL9wGyBqL-0aMnFfQJwnY21ZV5YKyDlPf5WfKNxUAoG0b0hYCSRp6XaghR-g44n29TycjXwvohc8H-xIf5ntc9g95YoHGdyvSul1vRCisNow/s320/images.jpeg",
+      source: "https://drive.google.com/uc?export=download&id=1iJG6RNykfMDFJIKiPt0WhGwuE1hCh4Z_"
+      },
+      {
+      name: "Inthandham",
+      artist: "Vishal Chandrasekhar",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/15.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/15.mp3"
+      }, 
+      {
+      name: "Skechers",
+      artist: "DripReprot",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/14.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/14.mp3"
+      },
+      {
+      name: "Taki Taki",
+      artist: "DJ Snake",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/13.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/13.mp3"
+    },
+    {
+      name: "Reddy gari ammai",
+      artist: "Afroz Ail",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/12.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/12.mp3"
+    },
+    {
+      name: "Tum Tum",
+      artist: "Thaman S",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/11.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/11.mp3"
+    }, 
+    {
+      name: "Toxic",
+      artist: "BoyWithUke",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/10.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/10.mp3"
+    },
+    {
+      name: "Faded",
+      artist: "Alan Walker",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/9.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/9.mp3"
+    }, 
+    {
+      name: "Live Fast",
+      artist: "Alan Walker & A$AP Rocky",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/1.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/1.mp3"
+    },
+    {
+      name: "Man On The Moon",
+      artist: "Alan Walker & Benjamin Ingrosso",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/2.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/2.mp3"
+    },
+    {
+      name: "On My Way",
+      artist: "Alan Walker, Farruko & Sabrina Carpenter",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/3.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/3.mp3"
+    },
+    {
+      name: "Sing Me To Sleep",
+      artist: "Alan Walker",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/4.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/4.mp3"
+    },
+    {
+      name: "Alone",
+      artist: "Alan Walker",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/5.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/5.mp3"
+    },
+    {
+      name: "Bewajah",
+      artist: "Himesh Reshammiya",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/6.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/6.mp3"
+    },
+    {
+      name: "Hymn For The Weekend",
+      artist: "Coldplay",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/7.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/7.mp3"
+    },
+    {
+      name: "Alone",
+      artist: "Marshmello",
+      cover: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/cover/8.png",
+      source: "https://raw.githubusercontent.com/raghavendratechnic/musicPlayer/main/music/8.mp3"
+    }
+  ]
